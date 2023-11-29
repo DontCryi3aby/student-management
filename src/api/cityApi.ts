fa7b7249650af857a7020301a333195405ede914
thachtryhard <i3oyhp@gmail.com>
2023-11-29 21:28:15 +0700

    Setup axios & api for project

---
+++ src/api/cityApi.ts
@@ -0,0 +1,16 @@
+import axiosClient from './axiosClient';
+import { City, ListResponse } from 'models';
+
+const cityApi = {
+    getAll(): Promise<ListResponse<City>> {
+        const url = '/cities';
+        return axiosClient.get(url, {
+            params: {
+                _limit: 10,
+                _page: 1,
+            },
+        });
+    },
+};
+
+export default cityApi;
